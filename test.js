1fcefc2c1b3d2651b7ddb7dfb7698852cc592734
JernKunpittaya <61564542+JernKunpittaya@users.noreply.github.com>
2023-04-13 17:27:15 -0400

    refine test.js

--- test.js
+++ test.js
@@ -1,18 +1,17 @@
 const lexical = require("./lexical");
 // let regex = "a(b|c)+d";
-function readM1(m1, layer, mem) {
+function readM1(m1, mem) {
   if (mem.includes(m1)) {
     console.log("exist already", m1);
     return;
   } else {
     mem.push(m1);
   }
-  //   console.log("layer: ", layer);
   console.log(m1);
 
   for (let i = 0; i < m1.edges.length; i++) {
     console.log("edge of ", m1.id, " : ", m1.edges[i][0]);
-    readM1(m1.edges[i][1], layer + 1, mem);
+    readM1(m1.edges[i][1], mem);
   }
 }
 
@@ -23,7 +22,7 @@ let submatches = [
 ];
 
 console.log("parsed: ", lexical.parseRegex(regex));
-// console.log("parsed: ", lexical.parseRegex(regex).parts[0].parts);
 console.log("detail");
-console.log(readM1(lexical.regexToM1(regex, submatches), 0, []));
+var M1 = lexical.regexToM1(regex, submatches);
+console.log(readM1(M1, []));
 //
